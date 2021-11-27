f89b6bbac0266ce271e4cb2534556375c17ad53c
lincoln <lincoln-md@hotmail.com>
2021-11-26 22:23:45 -0300

    commit

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Lincoln'
+    },
+    {
+        id: 2,
+        name: 'Lucia'
+    },
+    {
+        id: 3,
+        name: 'Silvia'
+    },
+]
